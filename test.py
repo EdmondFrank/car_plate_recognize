019db0ed113ce0e50474b047eb76628eb8df8d71
Edmond <edmomdfrank@yahoo.com>
2018-01-18 09:38:09 +0800

    add unitest support

--- test.py
+++ test.py
@@ -15,6 +15,17 @@ class TestTrain(unittest.TestCase):
         print("test_vec2text")
         test = text2vec("云F71K64")
         self.assertEqual("云F71K64", vec2text(test))
+    def test_text2vec(self):
+        print("test_text2vec")
+        vec = text2vec("云F71K64")
+        self.assertEqual(1, vec[43+68*0])
+        self.assertEqual(1, vec[15+68*1])
+        self.assertEqual(1, vec[7+68*2])
+        self.assertEqual(1, vec[1+68*3])
+        self.assertEqual(1, vec[20+68*4])
+        self.assertEqual(1, vec[6+68*5])
+        self.assertEqual(1, vec[4+68*6])
+        #print(vec)
 
 if __name__ == '__main__':
     unittest.main()
